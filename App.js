99de28ec458410d3d2a1560dcc90f1b00cc9fa0b
jeeva-nikam <jeevann87@gmail.com>
2025-08-04 23:30:55 +0530

    React basic create eleemnt and render element.

---
+++ App.js
@@ -0,0 +1,7 @@
+const el = React.createElement('div', { id: "parent" },
+    [React.createElement('h1', { id: "heading" }, 'This is heading')]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("demo"));
+console.log("root", root)
+root.render(el);
