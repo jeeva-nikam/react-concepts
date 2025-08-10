0016a84b8846def8ad5f0dd32d5d476791ccfdbf
jeeva-nikam <jeevann87@gmail.com>
2025-08-10 22:58:59 +0530

    Using useState and npm start

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,13 +1,24 @@
 import { cardList } from "../enums/constants"
 import Card from "./Card"
+import { useState } from "react"
 
 const CardContainer = () => {
+    const [listOfRes, setListOfRes] = useState(cardList)
     return (
-        <div className="card-container">
-            {cardList.map((cardObj, index)=>{
+        <div>
+            <button onClick={()=>{
+                const filteredList = cardList.filter(
+                    (res)=>res.info.avgRating > 4.5
+                )
+                setListOfRes(filteredList);
+            }}>Top rated restaurancts</button>
+            <div className="card-container">
+            {listOfRes.map((cardObj, index)=>{
                return <Card cardData = {cardObj} key={cardObj.info.id}/>
             })}
         </div>
+        </div>
+        
     )
 }
 
